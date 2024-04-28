73fc983ee2b41a05888e56fdce6a7f206363deb3
rootlee <irootlee@gmail.com>
2024-04-28 13:25:12 +0800

    update

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -3,7 +3,7 @@ function M.config()
     require 'nvim-treesitter.configs'.setup {
         -- 安装 language parser
         -- :TSInstallInfo 命令查看支持的语言
-        ensure_installed = { "c", "rust", "go", "html", "vim", "lua", "javascript", "typescript", "tsx", "ini" },
+        ensure_installed = { "c", "cpp", "rust", "go", "html", "vim", "lua", "javascript", "typescript", "tsx", "ini" },
         -- 启用代码高亮功能
         highlight = {
             enable = true,
